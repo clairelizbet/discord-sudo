6a81111c0e72df1303ff51f571b2d15d123a23a6
claire <clairelizbet@hey.com>
2022-11-03 23:23:52 -0700

    Ensure number values are properly converted to integers from floats

--- util/number.ts
+++ util/number.ts
@@ -1,15 +1,18 @@
+/**
+ * Returns an int value if one can be parsed
+ * from the given source value. If provided a float,
+ * it returns the floor of the value (e.g. 1.9 → 1)
+ */
 function intFromValue(
   sourceValue?: string | number | boolean,
   radix: number = 10
 ): number | undefined {
-  let parsedInteger: number = NaN
-
   if (typeof sourceValue === 'undefined') return sourceValue
   if (typeof sourceValue === 'boolean') return sourceValue ? 1 : 0
-  if (typeof sourceValue === 'number') parsedInteger = sourceValue
-  if (typeof sourceValue === 'string')
-    parsedInteger = parseInt(sourceValue, radix)
+  if (typeof sourceValue === 'number') return Math.floor(sourceValue)
 
+  // Source is a string - we'll try to parse it, returning undefined if unable
+  const parsedInteger: number = parseInt(sourceValue, radix)
   return isNaN(parsedInteger) ? undefined : parsedInteger
 }
 
